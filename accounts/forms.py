bf6bff5750d50060bd3522265436a5af51835162
Veronique SQ <fizzbones@gmail.com>
2018-11-17 18:36:45 +0000

    form validation - register

--- accounts/forms.py
+++ accounts/forms.py
@@ -12,12 +12,11 @@ class UserLoginForm(forms.Form):
     password = forms.CharField(widget=forms.PasswordInput)
     
 class UserRegistrationForm(UserCreationForm):
-    """
-    Form used to register a new user
-    """
+    """Form used to register a new user"""
 
-    password1 = forms.CharField(label="Password",
-    widget=forms.PasswordInput)
+    password1 = forms.CharField(
+        label="Password",
+        widget=forms.PasswordInput)
     password2 = forms.CharField(
         label="Password Confirmation",
         widget=forms.PasswordInput)
@@ -25,20 +24,22 @@ class UserRegistrationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['email', 'username', 'password1', 'password2']
-        
+    
     def clean_email(self):
         email = self.cleaned_data.get('email')
         username = self.cleaned_data.get('username')
         if User.objects.filter(email=email).exclude(username=username):
             raise forms.ValidationError(u'Email address must be unique')
         return email
-        
+
     def clean_password2(self):
         password1 = self.cleaned_data.get('password1')
         password2 = self.cleaned_data.get('password2')
+
         if not password1 or not password2:
             raise ValidationError("Please confirm your password")
-        if password2 != password2:
-            raise ValidationError("The passwords must match")
-            
+        
+        if password1 != password2:
+            raise ValidationError("Passwords must match")
+        
         return password2
