4a5f4f840a20d3269daae290189b74f852266b0d
Veronique SQ <fizzbones@gmail.com>
2018-11-17 14:22:58 +0000

    adding login logic and validation of input

--- accounts/views.py
+++ accounts/views.py
@@ -21,7 +21,17 @@ def login(request):
     """
     Return the login page
     """
-    login_form = UserLoginForm()
-
+    if request.method == "POST":
+        login_form = UserLoginForm(request.POST)
+        if login_form.is_valid():
+            user=auth.authenticate(username=request.POST['username'],
+            password=request.POST['password'])
+            messages.success(request, "You are now logged in")
+            if user:
+                auth.login(user=user, request=request)
+                else:
+                    login_form.add_error(None, "Your username or password is incorrect")
+    else:
+        login_form = UserLoginForm()
     return render(request, "login.html", {"login_form": login_form})
     
