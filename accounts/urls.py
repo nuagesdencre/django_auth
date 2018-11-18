8588d6fff0028741735ad79f1baf93a5bd9a69f8
Veronique SQ <fizzbones@gmail.com>
2018-11-18 08:14:08 +0000

    password reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^logout/', logout, name="logout"),
     url(r'^login/', login, name="login"),
     url(r'^register/', register, name="register"),
-    url(r'^profile/', user_profile, name="profile"),
+    url(r'^user_profile/', user_profile, name="user_profile"),
     url(r'^password-reset/', include(url_reset))
     ]
